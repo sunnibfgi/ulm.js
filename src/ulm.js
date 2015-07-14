00f5329bbe77824b682a087c25f7410f9a9c8f21
sunnibfgi <lebroncarmleo1523@gmail.com>
2015-07-14 14:54:18 +0800

    scroll event tester

---
+++ src/ulm.js
@@ -0,0 +1,45 @@
+//ulm.js
+
+;(function() {
+  var $window = $(window),
+    body = $(document.body),
+    original = body.scrollTop(),
+    hold = 300,
+    timer, step = 0;
+  body.on('show:alert', function() {
+    if(!step && original >= hold) {
+      alert('>>>>>>>>>>>>');
+      step = 1
+    }
+  });
+
+  function css(el, color) {
+    return el.css({
+      backgroundColor: color
+    })
+  }
+  
+  $window.on('scrollevent', function() {
+    var top = body.scrollTop();
+    if(timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function() {
+      if(top <= hold) {
+        css(body, '').trigger('show:alert')
+      } else if(top < original) {
+        step = 0;
+        clearTimeout(timer);
+        css(body, 'purple')
+      } else {
+        css(body, '')
+      }
+      original = top
+    }, 1)
+  }).on('scroll', function(e) {
+	  
+    if(original !== body.scrollTop()) {
+      $(this).trigger('scrollevent')
+    }
+  })
+})()
